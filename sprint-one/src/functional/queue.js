5025675fbe42b22d356d3460ed47b9e773fee440
Trevor Puccini <trevorpuccini21@gmail.com>
2020-03-25 12:57:25 -0700

    Add functionality to Queue constructor's enqueue, dequeue, and size methods

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,25 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var storProperties = [];
+    for (var key in storage) {
+      storProperties.push(storage[key]);
+    }
+    storProperties.unshift(value);
+    for (var i = 0; i < storProperties.length; i++) {
+      storage[i] = storProperties[i];
+    }
   };
 
   someInstance.dequeue = function() {
+    var keyToDequeue = Object.keys(storage).length - 1;
+    var valToDequeue = storage[keyToDequeue];
+    delete storage[keyToDequeue];
+    return valToDequeue;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
