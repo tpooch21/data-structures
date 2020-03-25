8a6fa409329af2d3a0a881af86d57b2439e2f84a
Trevor Puccini <trevorpuccini21@gmail.com>
2020-03-25 15:33:40 -0700

    Define push and pop functions within stackMethods object

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -6,19 +6,30 @@ var Stack = function() {
   var storage = {};
 
   // Rather than defining each method within the body of Stack(), reference methods in stackMethods object
-  someInstance.push =
+  someInstance.push = stackMethods.push;
 
 };
 
 
 // Define methods within stackMethods so that interpreter only visits them once
 var stackMethods = {
-  push: function() {
-
+  // Can interact with storage object due to closure created when Stack() is run
+  push: function(value) {
+    // Determine length of Object.keys(), and store in var newKey (length should equal next key to be added)
+    var newKey = Object.keys(storage).length;
+    // Add property to storage, with key newKey and value assigned to input string
+    storage[newKey] = value;
   },
 
   pop: function() {
-
+    // To determine what to delete from storage object, find last item using Object.keys().length - 1, which represents lastKey
+    var lastKey = Object.keys(storage).length - 1;
+    // Store the value at the lastKey in var valToPop
+    var valToPop = storage[lastKey];
+    // Delete property at lastKey
+    delete storage[lastKey];
+    // Return valToPop
+    return valToPop;
   },
 
   size: function() {
