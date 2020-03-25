d306984ef44d72ef275c881e95f34ed73da23bb3
Trevor Puccini <trevorpuccini21@gmail.com>
2020-03-25 15:24:05 -0700

    Create object outside of Stack() constructor to store methods

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  var storage = {};
+
+  // Rather than defining each method within the body of Stack(), reference methods in stackMethods object
+  someInstance.push =
+
 };
 
-var stackMethods = {};
+
+// Define methods within stackMethods so that interpreter only visits them once
+var stackMethods = {
+  push: function() {
+
+  },
+
+  pop: function() {
+
+  },
+
+  size: function() {
+
+  }
+
+};
 
 
