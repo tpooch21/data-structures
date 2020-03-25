ca1c46775b43cbc29a488989f75383e58dd83a53
Trevor Puccini <trevorpuccini21@gmail.com>
2020-03-25 12:32:26 -0700

    Add functionality to Stack constructor's push, pop, and size methods

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,13 +6,26 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var numOfKeys = Object.keys(storage).length;
+    var keyToAdd = numOfKeys;
+    storage[keyToAdd] = value;
   };
 
   someInstance.pop = function() {
+    var keyToPop = (Object.keys(storage).length - 1).toString();
+    var valToPop = storage[keyToPop];
+    delete storage[keyToPop];
+    return valToPop;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
 };
+
+
+
+var pancake = Stack();
+// pancake = someInstance = {}.push()
