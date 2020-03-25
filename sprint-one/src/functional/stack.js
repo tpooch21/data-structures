d00fc65fa4c9b297b2d4e463800f971e90bccc28
Trevor Puccini <trevorpuccini21@gmail.com>
2020-03-25 14:58:20 -0700

    Add pseudocode to method definitions

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,15 +6,22 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // Find number of keys by calling Object.keys().length
     var numOfKeys = Object.keys(storage).length;
+    // Since keys represent array indices starting at 0, numOfKeys will represent new key to add
     var keyToAdd = numOfKeys;
+    // Add key, with input string as value
     storage[keyToAdd] = value;
   };
 
   someInstance.pop = function() {
+    // Find the last key and attach var keyToPop to it (keys represent index values so can be treated same as finding last index of an array)
     var keyToPop = (Object.keys(storage).length - 1).toString();
+    // Save value at last key in var valToPop
     var valToPop = storage[keyToPop];
+    // Delete property at last key
     delete storage[keyToPop];
+    // Return value at last key
     return valToPop;
   };
 
@@ -26,6 +33,3 @@ var Stack = function() {
 };
 
 
-
-var pancake = Stack();
-// pancake = someInstance = {}.push()
