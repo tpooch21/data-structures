14eb915e67c6017dffc68766a80db6c28c3e9fe0
Trevor Puccini <trevorpuccini21@gmail.com>
2020-03-25 17:44:34 -0700

    Define push, pop, and size functions to include 'this' keyword

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,41 +1,46 @@
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
 
-  var storage = {};
+  someInstance.storage = {};
 
   // Rather than defining each method within the body of Stack(), reference methods in stackMethods object
-  someInstance.push = stackMethods.push;
+  _.extend(someInstance, Stack.stackMethods);
+
+  return someInstance;
 
 };
 
-
 // Define methods within stackMethods so that interpreter only visits them once
-var stackMethods = {
-  // Can interact with storage object due to closure created when Stack() is run
+Stack.stackMethods = {
+
   push: function(value) {
     // Determine length of Object.keys(), and store in var newKey (length should equal next key to be added)
-    var newKey = Object.keys(storage).length;
+    var newKey = Object.keys(this.storage).length;
     // Add property to storage, with key newKey and value assigned to input string
-    storage[newKey] = value;
+    this.storage[newKey] = value;
   },
 
   pop: function() {
     // To determine what to delete from storage object, find last item using Object.keys().length - 1, which represents lastKey
-    var lastKey = Object.keys(storage).length - 1;
+    var lastKey = Object.keys(this.storage).length - 1;
     // Store the value at the lastKey in var valToPop
-    var valToPop = storage[lastKey];
+    var valToPop = this.storage[lastKey];
     // Delete property at lastKey
-    delete storage[lastKey];
+    delete this.storage[lastKey];
     // Return valToPop
     return valToPop;
   },
 
   size: function() {
-
+    // Return length of Object.keys()
+    return Object.keys(this.storage).length;
   }
-
 };
+
+var pancake = Stack();
+console.log(pancake);
 
 
