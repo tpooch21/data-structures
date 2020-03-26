361f6106dafabad9e4289dd448d4f26edd392eb3
Trevor Puccini <trevorpuccini21@gmail.com>
2020-03-25 21:56:48 -0700

    Create variable within Stack definition that delegates to prototype stackMethods

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,6 +1,13 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // Create an object that delegates to prototype stackMethods
+  var someInstance = Object.create(stackMethods);
+  // Create a storage property on the someInstance object, with an object literal as its value
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
 var stackMethods = {};
