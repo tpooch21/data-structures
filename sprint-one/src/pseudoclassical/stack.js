5f91a94ddaa6c200bbef84f4beac9a71cd721f73
Trevor Puccini <trevorpuccini21@gmail.com>
2020-03-25 22:55:47 -0700

    Instantiate Stack object using 'new'
    Create a new instance of Stack using 'new' keyword. Within Stack constructor, add storage property to object created via 'new' keyword. Add push method to
    Stack.prototype.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,18 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // Use 'this' to refer to object created by 'new', and create properties on 'new' object
+  this.storage = {};
 };
 
+// Instantiate Stack object using 'new'
+var stack = new Stack();
 
+// Add methods to Stack prototype
+Stack.prototype.push = function(value) {
+  // Determine length of storage object and assign var lastKey to it
+  var lastKey = Object.keys(this.storage).length;
+  // Add prop lastKey with value of input value
+  this.storage[lastKey] = value;
+};
