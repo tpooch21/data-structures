a5bf2e9b1007432410875fee52ab0cc5229c9b00
Trevor Puccini <trevorpuccini21@gmail.com>
2020-03-25 23:02:35 -0700

    Add pop and size methods to Stack.prototype

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -11,8 +11,23 @@ var stack = new Stack();
 
 // Add methods to Stack prototype
 Stack.prototype.push = function(value) {
-  // Determine length of storage object and assign var lastKey to it
-  var lastKey = Object.keys(this.storage).length;
+  // Determine length of storage object and assign var keyToAdd to it
+  var keyToAdd = Object.keys(this.storage).length;
   // Add prop lastKey with value of input value
-  this.storage[lastKey] = value;
+  this.storage[keyToAdd] = value;
+};
+
+Stack.prototype.pop = function() {
+  // Find last key in storage, keep in mind keys represent array indices
+  var lastKey = Object.keys(this.storage).length - 1;
+  // Find value at last key and store in lastValue
+  var lastValue = this.storage[lastKey];
+  // Delete property at last key
+  delete this.storage[lastKey];
+  // Return lastValue
+  return lastValue;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
 };
