5aa5aa1bfc293b7a77305c320b419f5a9426a7fc
Trevor Puccini <trevorpuccini21@gmail.com>
2020-03-27 11:01:05 -0700

    transfer(sprint-two graph): updating

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -31,51 +31,125 @@
 // iterate over edges
 // edges[1][2]
 
-// [{1,2}, {}, {}]
+// [{2}, {3}, {}]
+// {'2': {4,1}}
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
 };
+
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  // this.nodes.push(node)
-  // this.nodes.push([])
+  // this.nodes[node] equal to a new set
+  if (!(this.nodes[node])) {
+    this.nodes[node] = new Set();
+  }
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  // Iterate through this.nodes and search for target node
-  // If found, return true
-  // Else return false
+  if (node in this.nodes) {
+    return true;
+  }
+  return false;
+  // for in loop to iterate over the keys
+  //    check the key by converting key into a number, if found: return true
+  // return false
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  // Get the index of the given node
-  // If index === -1, return;
-  // [{3}, {0}, {1}]
-  // var set = edges[index].keys()
-  // while (set.next) {}
+  // Iterate through the keys of the set at the node being passed in
+  var edges = this.nodes[node];
+  edges.forEach(function(key) {
+    this.nodes[key].delete(node);
+  });
+  // var result =
+  // while ()
+  //    access those keys in our dictionary
+  //    remove the node from their sets
+  // remove the property at
+  delete this.nodes[node];
+
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (!this.nodes[fromNode] || !this.nodes[toNode]) {
+    return false;
+  }
+  // we can do a lookup fromNode in our nodes dictionary
+  var edges = this.node[fromNode];
+  //  if toNode is in set
+  if (edges.has(toNode)) {
+    return true;
+  }
+  return false;
+  //      return true
+  // return false
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  //  if the value at key fromNode or value at key toNode are undefined: return;
+  if (!this.nodes[fromNode] || !this.nodes[toNode]) {
+    return;
+  }
+  //  do a lookup fromnode in our nodes dictionary
+  var fromNodeEdges = this.nodes[fromNode];
+  var toNodeEdges = this.nodes[toNode];
+
+  // if our fromNode edge does not have toNode add conneciton to both node edges
+  if (!(fromNodeEdges.has(toNode))) {
+    fromNodeEdges.add(toNode);
+    toNodeEdges.add(fromNode);
+  }
+  //  if toNode not in set edges
+  //    add toNode
+  //    lookup on toNode
+  //    add fromNode to toNode's set
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  //  if the value at key fromNode or value at key toNode are undefined: return;
+  if (!this.nodes[fromNode] || !this.nodes[toNode]) {
+    return;
+  }
+  //  lookup fromNode in our nodes dictionary
+  var fromNodeEdges = this.nodes[fromNode];
+  var toNodeEdges = this.nodes[toNode];
+  //  if toNode is in fromNode set
+  if (fromNodeEdges.has(toNode)) {
+    fromNodeEdges.delete(toNode);
+    toNodeEdges.delete(fromNode);
+  }
+  //    then remove toNode
+  //   lookup toNode in our nodes dictionary
+  //   remove fromNode
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  //  iterate over the keys using a for in loop
+  //  call the cb on numeric value of each key
+  for (var key in this.nodes) {
+    cb(parseInt(key));
+  }
 };
 
+
 /*
- * Complexity: What is the time complexity of the above functions?
+
+  addNode: O(1)
+  contains: O(n)
+  removeNode: O(1)
+  hasEdge: O(1)
+  addEdge: O(1)
+  removeEdge: O(1)
+  forEachNode: O(n) * Complexity: What is the time complexity of the above functions?
  */
+
 
 
