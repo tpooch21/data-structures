14d9e7412bb2fa096e84b70776fc5f5474647842
Trevor Puccini <trevorpuccini21@gmail.com>
2020-03-28 14:24:48 -0700

    Create class BinarySearchTree and define methods to be used by BST objects

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -2,13 +2,36 @@ describe('binarySearchTree', function() {
   var binarySearchTree;
 
   beforeEach(function() {
-    binarySearchTree = BinarySearchTree(5);
+    binarySearchTree = new BinarySearchTree(5);
+  });
+
+  it('should return undefined if new BinarySearchTree is created with null', function () {
+    expect(BinarySearchTree.bind(null, null)).to.throw('Parameter is not a valid value!');
   });
 
   it('should have methods named "insert", "contains", and "depthFirstLog', function() {
     expect(binarySearchTree.insert).to.be.a('function');
     expect(binarySearchTree.contains).to.be.a('function');
     expect(binarySearchTree.depthFirstLog).to.be.a('function');
+  });
+
+  it('should have a root', function () {
+    expect(binarySearchTree.contains(5)).to.equal(true);
+  });
+
+  it('should not do anything when inserting a duplicate node', function () {
+    binarySearchTree.insert(5);
+    expect(binarySearchTree.left).to.equal(null);
+    expect(binarySearchTree.right).to.equal(null);
+  });
+
+  it('should not treat negative nodes any differently than positive nodes', function () {
+    binarySearchTree.insert(-5);
+    expect(binarySearchTree.left.value).to.equal(-5);
+  });
+
+  it ('should not do anything when inserting null value', function() {
+    expect(binarySearchTree.insert.bind(binarySearchTree, null)).to.throw('Parameter is not a valid value!');
   });
 
   it('should insert values at the correct location in the tree', function() {
@@ -28,6 +51,12 @@ describe('binarySearchTree', function() {
     expect(binarySearchTree.contains(8)).to.equal(false);
   });
 
+  it('should have a "contains" method that returns true if tree has single node that equals input value', function() {
+    binarySearchTree.insert(5);
+    expect(binarySearchTree.contains(5)).to.equal(true);
+  });
+
+
   it('should execute a callback on every value in a tree using "depthFirstLog"', function() {
     var array = [];
     var func = function(value) { array.push(value); };
@@ -38,3 +67,18 @@ describe('binarySearchTree', function() {
     expect(array).to.eql([5, 2, 3, 7]);
   });
 });
+
+// contains:
+//    check for non existent value
+//    check the first node in the tree
+//    check the leaf of a tree
+
+// insert:
+//    insert one node: expect the tree to have 1 node
+//    insert a smaller value than the root: expect left child
+//    insert a greater value than the root: expect node to be a right child
+//    insert a value === to the node: should do nothing
+//    insert null: should do nothing
+//    insert a negative number
+
+
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,106 @@
 var BinarySearchTree = function(value) {
+  // BST: is a tree data structure with nodes in which right child node of the given node have a higher value and left child node will have a lower value
+  //  The left child node will always be smaller than the parent and sibling node to the right.
+  //  The right child node will always be greater than the parent node and the sibling node to the left
+  //  Each child node can be a root of a BST
+  //  A child node that is a leaf means they have no children
+  if (!value) {
+    throw 'Parameter is not a valid value!';
+  }
+
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
 
+
+// contruction details:
+//    node value
+//    node children
+
+//  Properties:
+//    left: has a node that is smaller than itself
+//    right: has a node that is greater than itself
+
+// Methods:
+
+//    insert(value): accepts a value and places it in the tree in the correct position
+//      compare our input value to this.value
+//      if input value > this.value:
+//        check if this.right is defined
+//        if undefined: set this.right to a new instance our of BST with input value
+//        else if defined: then call our insert function on this.right with the same input value
+//      else if input value < this.value:
+//        check if this.left is defined
+//        if undefined: set this.left to a new instance of our BST with input value
+//        else if defined: then call our function on this.left with the same input value
+//      else if input value === this.value:
+//        return undefined; --- do nothing
+
+BinarySearchTree.prototype.insert = function(value) {
+  if (!value) {
+    throw 'Parameter is not a valid value!';
+  }
+
+  if (value > this.value) {
+    if (!this.right) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  } else if (value < this.value) {
+    if (!this.left) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  }
+};
+
+//    contains: accepts a value and returns a boolean, reflecting whether the value is contained in the tree
+//        compare the input value to this.value
+//        if input value is greater: call contains on this.right
+//        else if input value is less than: call contains on this.left
+//        else if input value is equal to this.value: return true
+//        else return false
+
+BinarySearchTree.prototype.contains = function (value) {
+  if (value === this.value) {
+    return true;
+  }
+
+  if (value > this.value && this.right) {
+    return this.right.contains(value);
+  } else if (value < this.value && this.left) {
+    return this.left.contains(value);
+  }
+
+  return false;
+};
+//    depthFirstLog(cb): accepts a callback, and executes it in every value in the tree
+//      postOrder:
+//        check left:  invoke depthFirstLog on left
+//        check right:  invoke depthFirstLog on right
+//        invoke cb with this.value
+
+BinarySearchTree.prototype.depthFirstLog = function (cb) {
+  cb(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(cb);
+  }
+
+};
+
+// Notes:
+//  this tree will be implemented in pseudoclassical
+
 /*
  * Complexity: What is the time complexity of the above functions?
+    insert: O(logn)
+    contains: O(logn)
+    depthFirstLog: O(n)
  */
